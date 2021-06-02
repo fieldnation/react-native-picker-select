8d5d7fc76576c5af3152b65d8a13071a70cae7c3
shovon ghosh <shovon1010@gmail.com>
2021-06-02 19:18:09 +0600

    PRO-2028

--- src/styles.js
+++ src/styles.js
@@ -63,8 +63,9 @@ export const defaultStyles = StyleSheet.create({
     },
     headlessAndroidPicker: {
         position: 'absolute',
-        width: '100%',
-        height: '100%',
+        top: 0,
+        left: 0,
+        right: -44,
         color: 'transparent',
         opacity: 0,
     },
